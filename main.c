87336f38b3122038482d54f555698d0d8b7b3461
Kieran McLean <kieran.mclean@uon.edu.au>
2019-04-02 02:57:50 +0000

    Started assignment

--- main.c
+++ main.c
@@ -1,8 +1,23 @@
+/*  Title: Rotation Cipher
+    Description: This code takes in a message and produces a cipher of that message using a Rotation Cipher.
+    Usage: Please enter the message you want to encrypt: lunchtime
+           The ciphered text is (key shift = 2) nwpejvkog
+*/
+
+
+
 #include<stdio.h>
+
+// Define my function
+void Cipher(char* plainText, int key);
 
 int main()
 {
-
-
-return 0;
+   int key = 2;
+   char plainText[150];
+   
+   printf("please enter the message that you want to encrypt: ");
+   scanf("%s", plainText);
+   
+   
 }
